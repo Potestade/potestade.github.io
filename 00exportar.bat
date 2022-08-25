80e1115b92f9669b9326dc1b8be804926b7a5638
Potestade <potestadeweb@gmail.com>
2022-08-25 16:59:35 -0300

    Website updates.

--- 00exportar.bat
+++ 00exportar.bat
@@ -1,5 +1,6 @@
 @ECHO  OFF
 CD %1
+git remote set-url origin https://Potestade:0Mkw@I9UK1FF=>|C@github.com/Potestade/potestade.github.io.git
 git add -A
 git commit -m "Website updates." -q
 git push origin main

490bbf3f08ab3cf954cb7d4bdcc48f48763235bf
Potestade Desenvolvimento Web <potestadeweb@gmail.com>
2023-10-12 18:51:30 -0300

    Controle JS preload, menu e back to top
    Controle JS preload, menu e back to top

--- assets/js/controle.js
+++ assets/js/controle.js
@@ -1,32 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let shouldScrollToFooter = false; // Variável de controle
+  // Simule um atraso de 2 segundos (2000 milissegundos)
+  setTimeout(function () {
+    // Remova a tela de preload e exiba o conteúdo principal
+    document.querySelector(".preload").style.display = "none";
+    document.querySelector(".content").style.display = "block";
+  }, 500);
 
+  let shouldScrollToFooter = false;
   const linkContato = document.getElementById("link-contato");
+  const topButton = document.getElementById("topButton");
+  const offcanvas = document.querySelector(".offcanvas");
+  const backtotop = document.querySelector('.back-to-top');
 
   linkContato.addEventListener("click", function (e) {
     e.preventDefault();
-
-    // Define a variável para rolar até o footer
     shouldScrollToFooter = true;
 
-    // Fecha o offcanvas padrão, se estiver aberto
-    const offcanvas = document.querySelector(".offcanvas.show");
-    if (offcanvas) {
+    if (offcanvas.classList.contains("show")) {
       const offcanvasInstance = new bootstrap.Offcanvas(offcanvas);
       offcanvasInstance.hide();
     }
   });
 
-  // Adiciona um ouvinte para quando o offcanvas for fechado
-  const offcanvas = document.querySelector(".offcanvas");
   offcanvas.addEventListener("hidden.bs.offcanvas", function () {
     if (shouldScrollToFooter) {
-      // Rola até o ID "footer" (onde o formulário está)
       const secaoFormulario = document.getElementById("footer");
       if (secaoFormulario) {
         secaoFormulario.scrollIntoView({ behavior: 'smooth' });
-        shouldScrollToFooter = false; // Reseta a variável
+        shouldScrollToFooter = false;
       }
     }
   });
+
+  topButton.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
+  (function () {
+    'use strict';
+
+    const select = (el, all = false) => {
+      el = el.trim();
+      if (all) {
+        return [...document.querySelectorAll(el)];
+      } else {
+        return document.querySelector(el);
+      };
+    };
+
+    const onscroll = (el, listener) => {
+      el.addEventListener('scroll', listener);
+    };
+
+    if (backtotop) {
+      const toggleBacktotop = () => {
+        backtotop.style.bottom = window.scrollY > 100 ? '20px' : '-60px';
+      };
+
+      window.addEventListener('load', toggleBacktotop);
+      onscroll(document, toggleBacktotop);
+    }
+  })();
 });

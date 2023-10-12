8c929d321291662ae5aee19c06638a3e4fa7ce37
Potestade Desenvolvimento Web <potestadeweb@gmail.com>
2023-10-12 20:09:35 -0300

    Zerando o site
    Zerando o site

--- assets/js/preload.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Simule um atraso de 2 segundos (2000 milissegundos)
-    setTimeout(function() {
-        // Remova a tela de preload e exiba o conteúdo principal
-        document.querySelector(".preload").style.display = "none";
-        document.querySelector(".content").style.display = "block";
-    }, 500);
-});

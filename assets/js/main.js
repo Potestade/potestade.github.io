f2bf9e89b7e5dfd4c10304eacb92d922824fd29b
Potestade Desenvolvimento Web <potestadeweb@gmail.com>
2023-10-15 23:02:07 -0300

    Upadte

--- assets/js/main.js
+++ assets/js/main.js
@@ -219,20 +219,3 @@
   new PureCounter();
 
 })()
-
-function handleIntersection(entries) {
-  entries.map((entry) => {
-    if (entry.isIntersecting) {
-      // Item has crossed our observation
-      // threshold - load src from data-src
-      entry.target.src = entry.target.dataset.src;
-      entry.target.classList.remove('lazyload');
-      // Job done for this item - no need to watch it!
-      observer.unobserve(entry.target);
-    }
-  });
-}
-
-const images = document.querySelectorAll('.lazyload');
-const observer = new IntersectionObserver(handleIntersection);
-images.forEach(image => observer.observe(image));
